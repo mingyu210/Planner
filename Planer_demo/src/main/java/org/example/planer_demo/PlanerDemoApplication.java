eddc76802d624bf82ae98b9b0bf272ffc2e5e0e8
jeongminb933 <65400179+jeongminb933@users.noreply.github.com>
2024-11-19 18:41:53 +0900

    Add files via upload

---
+++ Planer_demo/src/main/java/org/example/planer_demo/PlanerDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.planer_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanerDemoApplication.class, args);
+	}
+
+}
